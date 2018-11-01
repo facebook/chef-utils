783bde53659857e5f44d39522d35ef8d009f54eb
Phil Dibowitz <phil@ipom.com>
2018-10-31 18:59:17 -0700

    Pass health check
    Reviewed By: NaomiReeves
    
    Differential Revision: D12878417
    
    fbshipit-source-id: bf92ffb79f14b83a1ba83a888daf749a6a4f1850

--- chefctl/sample_configs/chefctl-config.rb
+++ chefctl/sample_configs/chefctl-config.rb
@@ -1,3 +1,17 @@
+# Copyright 2013-present Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This config file is located at /etc/chefctl-config.rb
 # You can change this location by passing `-C/--config` to `chefctl`
 # Default options and descriptions are in comments below.
