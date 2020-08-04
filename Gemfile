8e5012c72714ffe0c1dfe9605618e9e20e182039
Phil Dibowitz <phil@ipom.com>
2020-08-04 15:51:26 -0700

    Add rubocop to CI; lint files
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem 'rubocop', '= 0.55'
--- chefctl/sample_hooks/chefctl_hooks.rb
+++ chefctl/sample_hooks/chefctl_hooks.rb
@@ -23,16 +23,16 @@
 
 module SampleHook
   # Used to initialize the hook
-  #def initialize
-  #end
+  # def initialize
+  # end
 
   # Called during command line option parsing.
   # Allows a plugin to define additional command-line arguments.
   # Parameters:
   # - parser: an OptionParser object
   # The return value is ignored.
-  #def cli_options(parser)
-  #end
+  # def cli_options(parser)
+  # end
 
   # Called between command line parsing, and acquiring the lock.
   # This hook is intended to be used to modify config options via
@@ -40,27 +40,27 @@ module SampleHook
   # Setup items for the chef run should be placed in pre_run, since
   # pre_start is called before the lock is acquired.
   # The return value is ignored.
-  #def pre_start
-  #end
+  # def pre_start
+  # end
 
   # Gets the hostname of the machine. This sets the HOSTNAME environment
   # variable for the chef-client process.
   # Returns the hostname of the machine as a string.
-  #def hostname
-  #end
+  # def hostname
+  # end
 
   # Validates the authenticity of chef certificates, regenerating
   # them if necessary.
   # The return value is ignored.
-  #def generate_certs
-  #end
+  # def generate_certs
+  # end
 
   # Called after the lock is acquired, before the chef run is started.
   # Parameters:
   # - output is the path to the log file for the chef run
   # The return value is ignored.
-  #def pre_run(output)
-  #end
+  # def pre_run(output)
+  # end
 
   # Called after the final chef run completes, before the lock is released.
   # Normally this would be after the first (and only) chef run, but
@@ -71,8 +71,8 @@ module SampleHook
   # - chef_exitcode is the exit code of the final chef-client process.
   #   (>0 on failure)
   # The return value is ignored.
-  #def post_run(output, chef_exitcode)
-  #end
+  # def post_run(output, chef_exitcode)
+  # end
 
   # Check whether or not another chef run is required.
   # Parameters:
@@ -82,8 +82,8 @@ module SampleHook
   # This hook is called at most `Chefctl::Config.max_retries` times.
   # With the default value of 1 retry, this hook is not called a second time,
   # regardless of the result of the chef re-run.
-  #def rerun_chef?(output, chef_exitcode)
-  #end
+  # def rerun_chef?(output, chef_exitcode)
+  # end
 end
 
 Chefctl::Plugin.register SampleHook
