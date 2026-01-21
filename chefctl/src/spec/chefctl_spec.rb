6012c08f65422b3357585ca4c1a615740ae0f6f9
dcrosby <dcrosby@meta.com>
2026-01-21 14:25:07 -0800

    [chef-utils] Add .editorconfig, remove vim modelines
    Summary:
    Copy editorconfig settings from between-meals and add patterns for
    extensionless scripts. Remove vim modelines from 4 files as the
    editorconfig now handles indentation settings.
    
    Test Plan:
    Verified editorconfig file created correctly and vim modelines removed.
    
    Reviewers:
    
    Tags:

--- chefctl/src/spec/chefctl_spec.rb
+++ chefctl/src/spec/chefctl_spec.rb
@@ -1,5 +1,3 @@
-# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
-#
 # Copyright 2024-present Meta Platforms, Inc. and affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
